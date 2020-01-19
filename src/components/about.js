14baa998ae812f662491b517580ed2f1710ae4a2
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2020-01-19 13:38:37 +0800

    updating the handles correctly

--- src/components/about.js
+++ src/components/about.js
@@ -2,36 +2,40 @@ import React from 'react';
 import portfolioData from './../data/portfolio';
 import LearnImage from './../img/learnbuildmeasure.png';
 const About = () => {
-	let { about_sections } = portfolioData;
-	return (
-		<section className="section-about" id="section-about">
-			<div className="u-center-text u-margin-bottom-big">
-				<h2 className="heading-secondary">
-					Build What Customers Love
-                                    </h2>
-			</div>
+  let { about_sections } = portfolioData;
+  return (
+    <section className="section-about" id="about-me">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">Build What Customers Love</h2>
+      </div>
 
-			<div className="row">
-				<div className="col-1-of-2">
-					{about_sections.map(section => {
-						return (<div key={section.name} className="section-about__list">
-							<h3 className="heading-tertiary u-margin-bottom-small highlight">{section.name}</h3>
-							<p className="paragraph">
-								{section.description}
-							</p>
-						</div>)
-					})}
+      <div className="row">
+        <div className="col-1-of-2">
+          {about_sections.map(section => {
+            return (
+              <div key={section.name} className="section-about__list">
+                <h3 className="heading-tertiary u-margin-bottom-small highlight">{section.name}</h3>
+                <p className="paragraph">{section.description}</p>
+              </div>
+            );
+          })}
 
-					<a href="#skill-features" className="btn-text">Learn more &rarr;</a>
-				</div>
-				<div className="col-1-of-2">
-					<div className="composition">
-						<img src={LearnImage} alt="Learn Build Measure" className="composition__photo composition__photo--p1" />
-					</div>
-				</div>
-			</div>
-		</section>
-	)
-}
+          <a href="#technical-skills" className="btn-text">
+            Read more &rarr;
+          </a>
+        </div>
+        <div className="col-1-of-2">
+          <div className="composition">
+            <img
+              src={LearnImage}
+              alt="Learn Build Measure"
+              className="composition__photo composition__photo--p1"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default About;
--- src/components/technicalskills.js
+++ src/components/technicalskills.js
@@ -7,84 +7,81 @@ import infra from './../img/cloud.jpeg';
 import technicalData from './../data/portfolio';
 
 const TechnicalSkill = () => {
-	let { technicalskill_sections } = technicalData;
-	let { frontend_skills, backend_skills, infrastructure_skills } = technicalskill_sections;
-	return (
-		<section className="section-stories">
-			<div className="bg-video">
-				<video className="bg-video__content" autoplay muted loop>
-					<source src={VideoMp4} type="video/mp4" />
-					<source src={VideoWebm} type="video/webm" />
-					Your browser is not supported!
-                    </video>
-			</div>
+  let { technicalskill_sections } = technicalData;
+  let { frontend_skills, backend_skills, infrastructure_skills } = technicalskill_sections;
+  return (
+    <section className="section-stories" id="technical-skills">
+      <div className="bg-video">
+        <video className="bg-video__content" autoplay muted loop>
+          <source src={VideoMp4} type="video/mp4" />
+          <source src={VideoWebm} type="video/webm" />
+          Your browser is not supported!
+        </video>
+      </div>
 
-			<div className="u-center-text u-margin-bottom-big">
-				<h2 className="heading-secondary">
-					Technical Areas of Expertise
-                </h2>
-			</div>
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">Technical Areas of Expertise</h2>
+      </div>
 
-			<div className="row">
-				<div className="story">
-					<figure className="story__shape">
-						<img src={frontEnd} alt="Person on a tour" className="story__img" />
-						<figcaption className="story__caption">Front End</figcaption>
-					</figure>
-					<div className="story__text">
-						<h3 className="heading-tertiary u-margin-bottom-small">Front End Technologies</h3>
+      <div className="row">
+        <div className="story">
+          <figure className="story__shape">
+            <img src={frontEnd} alt="Person on a tour" className="story__img" />
+            <figcaption className="story__caption">Front End</figcaption>
+          </figure>
+          <div className="story__text">
+            <h3 className="heading-tertiary u-margin-bottom-small">Front End Technologies</h3>
 
-						<div className="story__list">
-							{frontend_skills.map(val => {
-								return <span class="story__skill">{val}</span>
-							})
-							}
-						</div>
-					</div>
-				</div>
-			</div>
+            <div className="story__list">
+              {frontend_skills.map(val => {
+                return <span class="story__skill">{val}</span>;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div className="row">
-				<div className="story">
-					<figure className="story__shape">
-						<img src={backEnd} alt="Person on a tour" className="story__img" />
-						<figcaption className="story__caption">Back End</figcaption>
-					</figure>
-					<div className="story__text">
-						<h3 className="heading-tertiary u-margin-bottom-small">BackEnd Technologies</h3>
-						<div className="story__list">
-							{backend_skills.map(val => {
-								return <span class="story__skill">{val}</span>
-							})
-							}
-						</div>
-					</div>
-				</div>
-			</div>
+      <div className="row">
+        <div className="story">
+          <figure className="story__shape">
+            <img src={backEnd} alt="Person on a tour" className="story__img" />
+            <figcaption className="story__caption">Back End</figcaption>
+          </figure>
+          <div className="story__text">
+            <h3 className="heading-tertiary u-margin-bottom-small">BackEnd Technologies</h3>
+            <div className="story__list">
+              {backend_skills.map(val => {
+                return <span class="story__skill">{val}</span>;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div className="row">
-				<div className="story">
-					<figure className="story__shape">
-						<img src={infra} alt="Person on a tour" className="story__img" />
-						<figcaption className="story__caption">Infra Skills</figcaption>
-					</figure>
-					<div className="story__text">
-						<h3 className="heading-tertiary u-margin-bottom-small">Deployment & Build Skills</h3>
-						<div className="story__list">
-							{infrastructure_skills.map(val => {
-								return <span class="story__skill">{val}</span>
-							})
-							}
-						</div>
-					</div>
-				</div>
-			</div>
+      <div className="row">
+        <div className="story">
+          <figure className="story__shape">
+            <img src={infra} alt="Person on a tour" className="story__img" />
+            <figcaption className="story__caption">Infra Skills</figcaption>
+          </figure>
+          <div className="story__text">
+            <h3 className="heading-tertiary u-margin-bottom-small">Deployment & Build Skills</h3>
+            <div className="story__list">
+              {infrastructure_skills.map(val => {
+                return <span class="story__skill">{val}</span>;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<div className="u-center-text u-margin-top-huge">
-				<a href="#skill-features" className="btn-text">Read More &rarr;</a>
-			</div>
-		</section>
-	)
-}
+      <div className="u-center-text u-margin-top-huge">
+        <a href="#skill-features" className="btn-text">
+          Read More &rarr;
+        </a>
+      </div>
+    </section>
+  );
+};
 
 export default TechnicalSkill;
