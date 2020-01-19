b019d714e6032ae1568983ceba559e170761bfff
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2020-01-19 13:45:43 +0800

    adding the logo link

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logoImage from './../img/logo-white.png';
 
 const Logo = () => {
-	return (
-		<div className="header__logo-box">
-			<img src={logoImage} alt="Logo" className="header__logo" />
-		</div>
-	)
-}
+  return (
+    <div className="header__logo-box">
+      <a href="http://rakeshcheekatimala.github.io/portfolio">
+        <img src={logoImage} alt="Logo" className="header__logo" />
+      </a>
+    </div>
+  );
+};
 
 export default Logo;
