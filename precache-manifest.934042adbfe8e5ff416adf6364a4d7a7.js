778747400c7e6bd6ac574665fec5b6297fe05134
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2020-01-19 13:16:33 +0800

    Updates

---
+++ precache-manifest.934042adbfe8e5ff416adf6364a4d7a7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7c5eca44c40b6d82e43763e4d147286",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6be2788d8a586feca0f1",
+    "url": "/portfolio/static/css/main.feaddecc.chunk.css"
+  },
+  {
+    "revision": "a492bff9f6f0a9979ed3",
+    "url": "/portfolio/static/js/2.633a0148.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.633a0148.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6be2788d8a586feca0f1",
+    "url": "/portfolio/static/js/main.3c8d8b46.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  },
+  {
+    "revision": "ee5d9760fd2664cbabc8b59466dbf257",
+    "url": "/portfolio/static/media/backend.ee5d9760.jpeg"
+  },
+  {
+    "revision": "df951fc6ff18e17dd6335079b4e4031b",
+    "url": "/portfolio/static/media/cloud.df951fc6.jpeg"
+  },
+  {
+    "revision": "3d02e2f7387da6868e2101334c9c67e3",
+    "url": "/portfolio/static/media/frontend.3d02e2f7.jpeg"
+  },
+  {
+    "revision": "6e09f4102b0d64adc38d21bd9c89a9a7",
+    "url": "/portfolio/static/media/hero.6e09f410.jpg"
+  },
+  {
+    "revision": "87d015b59c1e4a81453afc9d3bcf180d",
+    "url": "/portfolio/static/media/learnbuildmeasure.87d015b5.png"
+  },
+  {
+    "revision": "40d4ef82deebd0521e91006d961ead40",
+    "url": "/portfolio/static/media/logo-white.40d4ef82.png"
+  },
+  {
+    "revision": "2797561ff2a3de15c850f2744b73b6d1",
+    "url": "/portfolio/static/media/nat-10.2797561f.jpg"
+  },
+  {
+    "revision": "61bbf12ae90a30cd432d77087921a02f",
+    "url": "/portfolio/static/media/nat-4.61bbf12a.jpg"
+  },
+  {
+    "revision": "19f9a22f45a15fbf80d420862bcadffb",
+    "url": "/portfolio/static/media/nat-5.19f9a22f.jpg"
+  },
+  {
+    "revision": "1ef6d764fc819e080d40aa72dfe03aa6",
+    "url": "/portfolio/static/media/nat-6.1ef6d764.jpg"
+  },
+  {
+    "revision": "5f846d16e6be9a73e6eb73f620379af2",
+    "url": "/portfolio/static/media/nat-7.5f846d16.jpg"
+  },
+  {
+    "revision": "f556a2c3f37c228f95a5eac3fae8c4a7",
+    "url": "/portfolio/static/media/video.f556a2c3.webm"
+  },
+  {
+    "revision": "ffcd876c5561db066c09f5922b384473",
+    "url": "/portfolio/static/media/video.ffcd876c.mp4"
+  }
+]);
