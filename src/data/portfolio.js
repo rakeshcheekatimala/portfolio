07e8553a11fc4690d383197567708da6c4dea1dd
rakeshcheekatimala <rakesh.cheekatimala@singtel.com>
2020-04-13 22:49:55 +0800

    updated my techstack details

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,28 +1,42 @@
 const data = {
-	"about_sections": [
-		{
-			"name": "About Me",
-			"description": "A Passionate Software Engineer with 7 years of experience who believes in building applications by applying design thinking principles, who embraces the best practices and agile methodologies. "
-		},
-		{
-			"name": "Adopt agile Practises",
-			"description": "Practices daily stand-up, retrospective, sprint planning, team alignment, and engagement. Product Iteration and delivery"
-		},
-		{
-			"name": "CI/CD",
-			"description": "Encourage & contribute for the setup & maintainence of CI/CD practises while working on any project."
-		},
-		{
-			"name": "Colloboration",
-			"description": "Effective Colloboration & Communication for working as a team for better delivery"
-		}
-	],
-	"technicalskill_sections": {
-		"frontend_skills": ["HTML", "CSS", "SASS", "REACT", "JAVASCRIPT", 'REDUX', 'ES6', 'MOBX', 'Cypress', 'Jest', 'StoryBook'],
-		"backend_skills": ["NODEJS", "JAVA", "SPRING", "EXPRESS", "MONGODB", "MYSQL", 'SWAGGER'],
-		"infrastructure_skills": ['AWS', 'Jenkins', 'TRAVIS CI', 'DOCKER', 'Webpack', 'Gulp', 'Nginx']
-	}
-
-
-}
+  about_sections: [
+    {
+      name: 'About Me',
+      description:
+        'A Passionate Software Engineer with 7 years of experience who believes in building applications by applying design thinking principles, who embraces the best practices and agile methodologies. ',
+    },
+    {
+      name: 'Adopt agile Practises',
+      description:
+        'Practices daily stand-up, retrospective, sprint planning, team alignment, and engagement. Product Iteration and delivery',
+    },
+    {
+      name: 'CI/CD',
+      description:
+        'Encourage & contribute for the setup & maintainence of CI/CD practises while working on any project.',
+    },
+    {
+      name: 'Colloboration',
+      description:
+        'Effective Colloboration & Communication for working as a team for better delivery',
+    },
+  ],
+  technicalskill_sections: {
+    frontend_skills: [
+      'HTML',
+      'CSS',
+      'SASS',
+      'REACT',
+      'JAVASCRIPT',
+      'REDUX',
+      'ES6',
+      'MOBX',
+      'Cypress',
+      'Jest',
+      'StoryBook',
+    ],
+    backend_skills: ['NODEJS', 'EXPRESS', 'MONGODB', 'MYSQL'],
+    infrastructure_skills: ['AWS', 'Jenkins', 'TRAVIS CI', 'DOCKER', 'Webpack', 'Gulp', 'Nginx'],
+  },
+};
 export default data;
